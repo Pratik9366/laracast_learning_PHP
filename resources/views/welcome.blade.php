f1df389bd5f124e9e9cd81e2008311c84789270b
Pratik9366 <supratikdas768@gmail.com>
2026-03-31 05:22:03 +0530

    (Learning): pass data to views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
 <x-layout title="Welcome Page">
     <h1>Welcome Page</h1>
+    <p>{{ $greetings }}, <?= $person ?> </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome', [
+    'greetings' => 'Hello',
+    'person' => request('person', 'Supratik'),
+]);
 
 Route::view('/about','about');
 
