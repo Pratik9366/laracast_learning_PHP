2c68da97edc3310e6708083967761bb0c1187235
Pratik9366 <supratikdas768@gmail.com>
2026-03-31 04:36:15 +0530

    (Learning): Layout Files

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Return home</h1>
-    <a href="/">Home</a>
-</body>
-</html>
+<x-layout title="About Us">
+    <h1>About Page</h1>
+</x-layout>
